d105ea3ef433f14192d0b8338f3c3ceb5526a89f
igor_alves <igornalves08@gmail.com>
2024-05-16 16:58:45 -0300

    novas alteracoes

--- src/Components/Products/style.ts
+++ src/Components/Products/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import theme from '../../global/style/theme';
 
 export const Conteiner = styled.View`
     align-items: center;
@@ -6,12 +7,18 @@ export const Conteiner = styled.View`
 
 export const ConteinerTop = styled.View`
     flex-direction: row;
+    align-items: center;
+    margin-top: 22px;
 `;
 
 export const Text = styled.Text`
-
+    font-family: ${theme.fonts.Subheading20};
+    font-size: 20px;
+    color: ${theme.colors.Black};
+    margin-right: 167px;
 `;
 
 export const TextSecundary = styled.Text`
-
+    font-family: ${theme.fonts.Body};
+    font-size: 14px;
 `;
